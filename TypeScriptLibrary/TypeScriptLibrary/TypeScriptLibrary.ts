05cedba9c466d90f62b11cbbd22a6a89da924eca
plasma_effect <dtyazsk_windows7@yahoo.co.jp>
2015-10-26 01:31:35 +0900

    文字を書いたりXorShiftれたりできるようにした

---
+++ TypeScriptLibrary/TypeScriptLibrary/TypeScriptLibrary.ts
@@ -0,0 +1,62 @@
+﻿namespace plasma {
+    export class XorShift {
+        constructor(private seed: number) {
+        }
+
+        public run() {
+            this.seed ^= (this.seed << 17);
+            this.seed ^= (this.seed >> 9);
+            this.seed ^= (this.seed << 8);
+            this.seed ^= (this.seed >> 27);
+            return this.seed;
+        }
+    }
+    export function make_xorshift(seed: number) {
+        var engine = new XorShift(seed);
+        return () => {
+            return engine.run();
+        };
+    }
+
+    export class CanvasTraits {
+        private canvas: HTMLCanvasElement;
+        private ctx: CanvasRenderingContext2D;
+        public flag: boolean;
+        constructor(field_name: string) {
+            this.canvas = <HTMLCanvasElement>document.getElementById(field_name);
+            if (!this.canvas || !this.canvas.getContext) {
+                this.flag = false;
+            }
+            else {
+                this.ctx = this.canvas.getContext('2d');
+                this.flag = true;
+            }
+        }
+
+        public draw_string(
+            str: string,
+            color: number[],
+            size: number,
+            offset_x: number,
+            offset_y: number
+        ) {
+            if (!this.flag) return;
+            this.ctx.fillStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
+            this.ctx.font = size + "px 'ＭＳ Ｐゴシック'";
+            this.ctx.fillText(str, offset_x, offset_y);
+        }
+        public draw_string_style(
+            str: string,
+            style: string,
+            size: number,
+            offset_x: number,
+            offset_y: number
+        ) {
+            if (!this.flag) return;
+            this.ctx.fillStyle = style;
+            this.ctx.font = size + "px 'ＭＳ Ｐゴシック'";
+            this.ctx.fillText(str, offset_x, offset_y);
+        }
+
+    }
+}
---
+++ TypeScriptLibrary/TypeScriptLibrary/app.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="TypeScriptLibrary.ts"/>
+
+window.onload = () => {
+    var c = new plasma.CanvasTraits("field");
+    var func = plasma.make_xorshift(1);
+    c.draw_string(func().toString(), [0, 0, 0], 16, 0, 16 * 1);
+    c.draw_string(func().toString(), [0, 0, 0], 16, 0, 16 * 2);
+    c.draw_string(func().toString(), [0, 0, 0], 16, 0, 16 * 3);
+    c.draw_string(func().toString(), [0, 0, 0], 16, 0, 16 * 4);
+};
