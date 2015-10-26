df6f86d4b254419167141201e8df578c04065ccd
plasma_effect <dtyazsk_windows7@yahoo.co.jp>
2015-10-26 17:25:13 +0900

    uniformed_int_distribution, CanvasTraits.draw_rect

--- TypeScriptLibrary/TypeScriptLibrary/TypeScriptLibrary.ts
+++ TypeScriptLibrary/TypeScriptLibrary/TypeScriptLibrary.ts
@@ -1,21 +1,47 @@
 ﻿namespace plasma {
-    export class XorShift {
-        constructor(private seed: number) {
-        }
-
-        public run() {
-            this.seed ^= (this.seed << 17);
-            this.seed ^= (this.seed >> 9);
-            this.seed ^= (this.seed << 8);
-            this.seed ^= (this.seed >> 27);
-            return this.seed;
+    export namespace utility {
+        var date = new Date();
+        export function get_time() {
+            return date.getTime();
         }
     }
-    export function make_xorshift(seed: number) {
-        var engine = new XorShift(seed);
-        return () => {
-            return engine.run();
-        };
+
+    export namespace random {
+        export class XorShift {
+            constructor(private seed: number) {
+            }
+
+            public run() {
+                this.seed ^= (this.seed << 17);
+                this.seed ^= (this.seed >> 9);
+                this.seed ^= (this.seed << 8);
+                this.seed ^= (this.seed >> 27);
+                return this.seed;
+            }
+        }
+        export function make_xorshift(seed: number) {
+            var engine = new XorShift(seed);
+            return () => {
+                return engine.run();
+            };
+        }
+        export function seed_generate() {
+            var v = utility.get_time();
+            v ^= (v << 19);
+            v ^= (v >> 13);
+            v ^= (v << 8);
+            v ^= (v >> 21);
+            return v;
+        }
+
+        export function uniformed_int_distribution(callback: () => number, min: number, max: number) {
+            return () => {
+                var x = callback();
+                var m = Math.floor((1 << 30) / (max - min + 1)) * (max - min + 1);
+                for (; x < 0 || x >= m; x = callback());
+                return (x % (max - min + 1)) + min;
+            };
+        }
     }
 
     export class CanvasTraits {
@@ -35,28 +61,26 @@
 
         public draw_string(
             str: string,
-            color: number[],
             size: number,
             offset_x: number,
-            offset_y: number
-        ) {
-            if (!this.flag) return;
-            this.ctx.fillStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
-            this.ctx.font = size + "px 'ＭＳ Ｐゴシック'";
-            this.ctx.fillText(str, offset_x, offset_y);
-        }
-        public draw_string_style(
-            str: string,
-            style: string,
-            size: number,
-            offset_x: number,
-            offset_y: number
+            offset_y: number,
+            style: string = "black"
         ) {
             if (!this.flag) return;
             this.ctx.fillStyle = style;
             this.ctx.font = size + "px 'ＭＳ Ｐゴシック'";
-            this.ctx.fillText(str, offset_x, offset_y);
+            this.ctx.fillText(str, offset_x, offset_y + size);
         }
 
+        public draw_rect(
+            offset_x: number,
+            offset_y: number,
+            width: number,
+            height: number,
+            style: string = "black") {
+            if (!this.flag) return;
+            this.ctx.fillStyle = style;
+            this.ctx.fillRect(offset_x, offset_y, width, height);
+        }
     }
 }
--- TypeScriptLibrary/TypeScriptLibrary/app.ts
+++ TypeScriptLibrary/TypeScriptLibrary/app.ts
@@ -2,9 +2,9 @@
 
 window.onload = () => {
     var c = new plasma.CanvasTraits("field");
-    var func = plasma.make_xorshift(1);
-    c.draw_string(func().toString(), [0, 0, 0], 16, 0, 16 * 1);
-    c.draw_string(func().toString(), [0, 0, 0], 16, 0, 16 * 2);
-    c.draw_string(func().toString(), [0, 0, 0], 16, 0, 16 * 3);
-    c.draw_string(func().toString(), [0, 0, 0], 16, 0, 16 * 4);
+    var engine = plasma.random.make_xorshift(plasma.random.seed_generate());
+    var dis = plasma.random.uniformed_int_distribution(engine, 1, 6);
+    c.draw_rect(0, 0, 32, 32);
+    c.draw_rect(0, 32, 32, 32, "red");
+
 };
